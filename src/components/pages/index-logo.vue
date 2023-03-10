2e3da6e739c68193e5bcee26edbad272203babb4
xcx <1255137205@qq.com>
2023-03-10 08:53:00 +0800

    first commit

---
+++ src/components/pages/index-logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--    logo-->
+  
+  <el-row class="my-el-row-2">
+    <el-col :span="16" :offset="4" class="my-el-col-2" style="margin-top: 40px;">
+      <!-- <img src="../../assets/collegeLogo.png" alt="湖北工业大学工程技术学院二手交易平台"> -->
+    </el-col>
+  </el-row>
+  <!--    logo结束-->
+</template>
+
+<script>
+    export default {
+        name: "index-logo"
+    }
+</script>
+
+<style scoped>
+  .my-el-row-2{
+    height: 260px;
+    background-color: rgb(242,241,234);
+    text-align: center;
+  }
+  .my-el-col-2{
+    padding-top: 50px;
+  }
+</style>
